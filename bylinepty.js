68814cf59af1f129921af0800c3c5327da7bf8a6
Yi Zhao <zhaoyi0113@gmail.com>
2017-08-04 11:53:31 +1000

    .

--- bylinepty.js
+++ bylinepty.js
@@ -1,6 +1,8 @@
 let pty = require('node-pty');
 const stripAnsi = require('strip-ansi');
-let ptyProcess = pty.spawn('mongo', [], {
+const LineStream = require('byline').LineStream;
+
+let ptyProcess = pty.spawn('bash', [], {
   name: 'xterm-color',
   cols: 80,
   rows: 30,
@@ -10,17 +12,12 @@ let ptyProcess = pty.spawn('mongo', [], {
 
 ptyProcess.on('data', function(data) {
   const o = stripAnsi(data);
-  process.stdout.write(o);
+
+  console.log(o);
 });
 ptyProcess.on('exit', function(data) {
   console.log('exit', data);
 });
 setTimeout(() => {
-  ptyProcess.write('db');
-}, 1000);
-setTimeout(() => {
-  ptyProcess.write('\r');
-}, 1000);
-
-
-// ptyProcess.write('db.getSiblingDB("admin").runCommand( { getParameter : "*" })');
+  ptyProcess.write('db.getSiblingDB("admin").runCommand( { getParameter : "*" })\r');
+}, 2000);
